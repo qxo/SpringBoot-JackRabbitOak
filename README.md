e138a9e77e6a4d0a32c5370c052f7d1852a8996c
qxo <49526356@qq.com>
2020-10-11 20:06:30 +0800

    feat: add gradle build and change database to h2

---
+++ README.md
@@ -0,0 +1,16 @@
+spring-boot-jackrabbit-oak demo
+========================================
+* maven way test:
+```
+mvn test
+```
+
+* gradle way test:
+```
+gradlew test
+```
+
+* upload file example:
+```
+curl -v -F file=@t1.txt "http://127.0.0.1:8080/upload"
+```
--- src/test/java/com/jackrabbit/ApplicationTests.java
+++ src/test/java/com/jackrabbit/ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.jackrabbit;
 
+import javax.jcr.Repository;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.jackrabbit.service.JackRabbitService;
+
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
+	/**
+	* Logger for this class.
+	*/
+	private static final Logger LOG = LoggerFactory.getLogger(ApplicationTests.class);
+	
+	@Autowired Repository repository;
+	@Autowired JackRabbitService service;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		LOG.info("repository={}", repository);
+		LOG.info("service={}", service);
+		System.out.println("done.");
+		//Assert.assertTrue(false);
 	}
 
 }
